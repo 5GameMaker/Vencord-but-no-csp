878beaf9fecd2b3fc87f11ad1ab9ca6cc524c695
buj <buj351@outlook.com>
2025-06-07 18:26:09 +0700

    csp: added vencord: url

--- src/main/csp.ts
+++ src/main/csp.ts
@@ -115,16 +115,16 @@ const patchCsp = (headers: PolicyMap) => {
 
         // headers[header] = [stringifyPolicy(csp)];
         headers[header] = [[
-            "default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'; ",
-            "script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; ",
-            "script-src-elem * data: blob: 'unsafe-inline' 'unsafe-eval';",
-            "connect-src * data: blob: 'unsafe-inline'; ",
-            "img-src * data: blob: 'unsafe-inline'; ",
-            "media-src * data: blob: 'unsafe-inline'; ",
-            "frame-src * data: blob: ; ",
-            "style-src * data: blob: 'unsafe-inline';",
-            "font-src * data: blob: 'unsafe-inline';",
-            "frame-ancestors * data: blob:;",
+            "default-src * data: mediastream: blob: filesystem: about: ws: wss: vencord: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'; ",
+            "script-src * data: blob: vencord: 'unsafe-inline' 'unsafe-eval'; ",
+            "script-src-elem * data: blob: vencord: 'unsafe-inline' 'unsafe-eval';",
+            "connect-src * data: blob: vencord: 'unsafe-inline'; ",
+            "img-src * data: blob: vencord: 'unsafe-inline'; ",
+            "media-src * data: blob: vencord: 'unsafe-inline'; ",
+            "frame-src * data: blob: vencord: ; ",
+            "style-src * data: blob: vencord: 'unsafe-inline';",
+            "font-src * data: blob: vencord: 'unsafe-inline';",
+            "frame-ancestors * data: blob: vencord:;",
         ].join(" ")];
     }
 };
